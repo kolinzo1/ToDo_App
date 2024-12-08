498730345ee90b37043b5717e8527c4af386251d
kolinzo1 <kolaoyedele@icloud.com>
2024-12-07 23:09:50 -0500

    added ContactForm component

---
+++ task-management-app/src/components/ContactForm.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+
+const ContactForm = () => {
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    comments: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        name="firstName"
+        value={formData.firstName}
+        onChange={handleChange}
+        placeholder="First Name"
+        required
+      />
+      <input
+        type="text"
+        name="lastName"
+        value={formData.lastName}
+        onChange={handleChange}
+        placeholder="Last Name"
+        required
+      />
+      <input
+        type="email"
+        name="email"
+        value={formData.email}
+        onChange={handleChange}
+        placeholder="Email"
+        required
+      />
+      <textarea
+        name="comments"
+        value={formData.comments}
+        onChange={handleChange}
+        placeholder="Comments"
+        required
+      ></textarea>
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+export default ContactForm;
